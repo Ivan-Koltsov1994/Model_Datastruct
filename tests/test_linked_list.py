d2249afec8e59e0bd1a63f98382425718c6dfa63
Ivan <ivan.koltsov.1994@mail.com>
2023-03-19 20:08:26 +0500

    Выполнил задание модуля 6: -Создал класс LinkedList -Реализовал методы to_list(),get_data_by_id() -Реализовал блоки try/except для перехвата ошибки TypeError -Написал необходимые тесты

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -9,11 +9,46 @@ class TestLinked(unittest.TestCase):
         """Тестируем, что данные ложатся в LinkedList"""
         ll = LinkedList()
         ll.insert_beginning({'id': 1})
-        ll.insert_beginning({'id': 2})
         assert ll.head is not None
+        self.assertEqual(ll.tail.next_node, None)
 
     def test_linked_insert_at_end_none(self):
         """Тестируем, что данные ложатся в LinkedList при пустой структуре данных"""
         ll = LinkedList()
         ll.insert_at_end({'id': 1})
         assert ll.head is not None
+
+    def test_linked_get_data_by_id(self):
+        """Тестируем получение первого найденного словаря по переданному значению"""
+
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+
+        self.assertEqual(ll.get_data_by_id(3), {'id': 3})
+        self.assertEqual(ll.get_data_by_id(4), None)
+
+    def test_check_linked_list(self):
+        """Тестируем правильность хранения ссылок"""
+
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+
+        self.assertEqual(ll.head.data, {'id': 0})
+        self.assertEqual(ll.tail.data, {'id': 3})
+        self.assertEqual(ll.head.next_node.data, {'id': 1})
+        self.assertEqual(ll.head.next_node.next_node.data, {'id': 2})
+        self.assertEqual(ll.head.next_node.next_node.next_node.data, {'id': 3})
+        self.assertEqual(ll.tail.next_node, None)
+
+    def test_linked_insert_incorrect_element(self):
+        """Ожидается обработка исключения TypeError при добавлении некорректного элемента"""
+        ll = LinkedList()
+        result = 'Данные не являются словарем или в словаре нет id.'
+        self.assertEqual(ll.insert_beginning([1, 2, 3]), print(result))
+        self.assertEqual(ll.insert_at_end({'one': 1}), print(result))
